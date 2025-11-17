3b70f4403e1e8e1850040318a3cb870f82dba36c
Anupam Nilav <anupam.nilav1121@gmail.com>
2025-11-17 22:37:23 +0530

    IPL-Dashboard-Day_1

---
+++ src/test/java/io/anupamnilav/IPL_Dashboard/IplDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.anupamnilav.IPL_Dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
