6613df74085b4b90085555eeb658b6bcf552e08a
Anupam Nilav <anupam.nilav1121@gmail.com>
2025-11-21 20:11:25 +0530

    IPL-Dashboard-Day_5

--- src/main/java/io/anupamnilav/IPL_Dashboard/data/JobCompletionNotificationListener.java
+++ src/main/java/io/anupamnilav/IPL_Dashboard/data/JobCompletionNotificationListener.java
@@ -2,9 +2,10 @@ package io.anupamnilav.IPL_Dashboard.data;
 
 
 import io.anupamnilav.IPL_Dashboard.model.Match;
+import io.anupamnilav.IPL_Dashboard.model.Team;
+import jakarta.persistence.EntityManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
@@ -13,25 +14,27 @@ import org.springframework.jdbc.core.DataClassRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class JobCompletionNotificationListener implements JobExecutionListener {
 
     private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
 
-    private final JdbcTemplate jdbcTemplate;
-    @Autowired
-    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    private final EntityManager em;
+    public JobCompletionNotificationListener(EntityManager em) {
+        this.em = em;
     }
 
     @Override
     public void afterJob(JobExecution jobExecution) {
         if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
             log.info("!!! JOB FINISHED! Time to verify the results");
-
-            jdbcTemplate
-                    .query("SELECT team1, team2 FROM match", new DataClassRowMapper<>(Match.class))
-                    .forEach(person -> log.info("Found <{}> in the database.", person));
+            List<Object[]> results=em.createQuery("select distinct m.team1,count(*) from match m group by m.team1")
+                                     .getResultList()
+                                     .stream()
+            Map<String, Team> teamData=new HashMap<>();
         }
     }
 }
---
+++ src/main/java/io/anupamnilav/IPL_Dashboard/model/Team.java
@@ -0,0 +1,54 @@
+package io.anupamnilav.IPL_Dashboard.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Team {
+    @Id
+    private long id;
+    private String teamName;
+    private long totalMatches;
+    private long totalWins;
+
+    public Team() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public long getTotalMatches() {
+        return totalMatches;
+    }
+
+    public void setTotalMatches(long totalMatches) {
+        this.totalMatches = totalMatches;
+    }
+
+    public long getTotalWins() {
+        return totalWins;
+    }
+
+    public void setTotalWins(long totalWins) {
+        this.totalWins = totalWins;
+    }
+
+    public Team(String teamName,long totalMatches) {
+        this.teamName = teamName;
+        this.totalMatches=totalMatches;
+    }
+}
